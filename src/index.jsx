f90116aebffbfa3ff1fff515357d0e88f4059a91
Stephen Thompson <thompson.stephen113@gmail.com>
2023-10-09 15:43:53 -0400

    updated index.jsx to import and return MainView

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
 
+import { MainView } from "./components/main-view/main-view";
+
 // Import index.scss to bundle
 import "./index.scss";
 
 // Main component
 const MyFlixApplication = () => {
     return (
-        <div className="my-flix">
-            <div>Good morning</div>
-        </div>
+        <MainView />
     );
 };
 
