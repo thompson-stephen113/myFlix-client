fb88428493a5fdee2201fe6727643a30a5291983
Stephen Thompson <thompson.stephen113@gmail.com>
2023-10-25 07:50:37 -0400

    created login-view component
    Includes form for existing users to POST to /users endpoint with username and password.

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            access: username,
+            secret: password
+        };
+
+        fetch("https://myflix-db-app-24338506cd5a.herokuapp.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Login response: ", data);
+            if (data.user) {
+                localStorage.setItem("user", JSON.stringify(data.user));
+                localStorage.setItem("token", data.token);
+                onLoggedIn(data.user, data.token);
+            } else {
+                alert("No such user");
+            }
+        })
+        .catch((e) => {
+            alert("Something went wrong");
+        });
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+            </label>
+            <br />
+
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </label>
+            <br />
+            
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
