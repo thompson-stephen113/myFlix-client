4cf69d4260e1cff6df422976aea8a1899aa68f5f
Stephen Thompson <thompson.stephen113@gmail.com>
2023-11-14 15:18:23 -0500

    created favorite-movies.jsx
    Returns favorites movies list in a card as an array.
    Includes a button to remove items from list.

---
+++ src/components/profile-view/favorite-movies.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Card, Col, Button, Figure } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import "./profile-view.scss";
+
+export const FavoriteMovies = ({ favoriteMovies, removeFavorite }) => {
+    return (
+        <Card>
+            <Card.Body>
+                <h2>Favorites</h2>
+                {favoriteMovies.map(({ ImagePath, Title, id }) => {
+                    return (
+                        <Col key={id} className="favorite-movie">
+                            <Figure>
+                                <Button variant="link" className="image-button">
+                                    <Link to={`/movies/${encodeURIComponent(Title)}`}>
+                                        <Figure.Image
+                                            src={ImagePath}
+                                            alt={[Title, " movie poster"]}
+                                        />
+                                    </Link>
+                                </Button>
+                                <Figure.Caption className="figure-caption">
+                                    {Title}
+                                </Figure.Caption>
+                            </Figure>
+
+                            <Button
+                                className="remove-button"
+                                variant="secondary"
+                                onClick={() => removeFavorite(id)}>
+                                    Remove
+                            </Button>
+                        </Col>   
+                    )
+                })} 
+            </Card.Body>                 
+        </Card>
+    )
+}
