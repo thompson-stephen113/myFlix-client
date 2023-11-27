2edfe5cf6d644d9a27a0385879105995cadd4112
Stephen Thompson <thompson.stephen113@gmail.com>
2023-11-27 12:46:47 -0500

    added collapsing behavior to navbar when a link is clicked

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import "./navigation-bar.scss";
+
 export const NavigationBar = ({ user, onLoggedOut }) => {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleToggle = () => {
+        setExpanded(!expanded);
+    };
+
+    const handleNav = () => {
+        setExpanded(false);
+    }
     return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand as={Link} to="/">
+        <Navbar bg="light" expand="lg" expanded={expanded} onToggle={handleToggle}>
+            <Container fluid>
+                <Navbar.Brand as={Link} to="/" onClick={handleNav}>
                     myFlix
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -13,23 +25,26 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                     <Nav className="me-auto">
                         {!user && (
                             <>
-                                <Nav.Link as={Link} to="/login">
+                                <Nav.Link as={Link} to="/login" onClick={handleNav}>
                                     Login
                                 </Nav.Link>
-                                <Nav.Link as={Link} to="/signup">
+                                <Nav.Link as={Link} to="/signup" onClick={handleNav}>
                                     Signup
                                 </Nav.Link>
                             </>
                         )}
                         {user && (
                             <>
-                                <Nav.Link as={Link} to="/">
+                                <Nav.Link as={Link} to="/" onClick={handleNav}>
                                     Home
                                 </Nav.Link>
-                                <Nav.Link as={Link} to="/profile">
+                                <Nav.Link as={Link} to="/profile" onClick={handleNav}>
                                     Profile
                                 </Nav.Link>
-                                <Nav.Link onClick={onLoggedOut}>
+                                <Nav.Link onClick={() => {
+                                    onLoggedOut();
+                                    handleNav();
+                                }}>
                                     Logout
                                 </Nav.Link>
                             </>
